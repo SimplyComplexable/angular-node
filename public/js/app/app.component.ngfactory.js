9b9ca109dc077ab4008c35eca556dfb7a989349e
zsunderland <simplycomplexable@gmail.com>
2017-05-09 11:25:40 -0600

    Prod changes

--- public/js/app/app.component.ngfactory.js
+++ public/js/app/app.component.ngfactory.js
@@ -5,13 +5,11 @@
  */
 /* tslint:disable */
 import * as import0 from '@angular/core';
-import * as import1 from './header.component.ngfactory';
-import * as import2 from './header.component';
-import * as import3 from '@angular/router';
-import * as import4 from './error/error.component.ngfactory';
-import * as import5 from './error/error.component';
-import * as import6 from './error/error.service';
-import * as import7 from './app.component';
+import * as import1 from '@angular/router';
+import * as import2 from './error/error.component.ngfactory';
+import * as import3 from './error/error.component';
+import * as import4 from './error/error.service';
+import * as import5 from './app.component';
 var styles_AppComponent = [];
 export var RenderType_AppComponent = import0.ɵcrt({
     encapsulation: 2,
@@ -20,18 +18,16 @@ export var RenderType_AppComponent = import0.ɵcrt({
 });
 export function View_AppComponent_0(l) {
     return import0.ɵvid(0, [
-        (l()(), import0.ɵeld(0, null, null, 7, 'div', [[
+        (l()(), import0.ɵeld(0, null, null, 5, 'div', [[
                 'class',
                 'container'
             ]
         ], null, null, null, null, null)),
         (l()(), import0.ɵted(null, ['\n    '])),
-        (l()(), import0.ɵeld(0, null, null, 1, 'app-header', [], null, null, null, import1.View_HeaderComponent_0, import1.RenderType_HeaderComponent)),
-        import0.ɵdid(24576, null, 0, import2.HeaderComponent, [], null, null),
         (l()(), import0.ɵted(null, ['\n    '])),
         (l()(), import0.ɵeld(8388608, null, null, 1, 'router-outlet', [], null, null, null, null, null)),
-        import0.ɵdid(73728, null, 0, import3.RouterOutlet, [
-            import3.RouterOutletMap,
+        import0.ɵdid(73728, null, 0, import1.RouterOutlet, [
+            import1.RouterOutletMap,
             import0.ViewContainerRef,
             import0.ComponentFactoryResolver,
             [
@@ -41,16 +37,16 @@ export function View_AppComponent_0(l) {
         ], null, null),
         (l()(), import0.ɵted(null, ['\n'])),
         (l()(), import0.ɵted(null, ['\n'])),
-        (l()(), import0.ɵeld(0, null, null, 1, 'app-error', [], null, null, null, import4.View_ErrorComponent_0, import4.RenderType_ErrorComponent)),
-        import0.ɵdid(57344, null, 0, import5.ErrorComponent, [import6.ErrorService], null, null)
+        (l()(), import0.ɵeld(0, null, null, 1, 'app-error', [], null, null, null, import2.View_ErrorComponent_0, import2.RenderType_ErrorComponent)),
+        import0.ɵdid(57344, null, 0, import3.ErrorComponent, [import4.ErrorService], null, null)
     ], function (ck, v) {
-        ck(v, 10, 0);
+        ck(v, 8, 0);
     }, null);
 }
 function View_AppComponent_Host_0(l) {
     return import0.ɵvid(0, [
         (l()(), import0.ɵeld(0, null, null, 1, 'my-app', [], null, null, null, View_AppComponent_0, RenderType_AppComponent)),
-        import0.ɵdid(24576, null, 0, import7.AppComponent, [], null, null)
+        import0.ɵdid(24576, null, 0, import5.AppComponent, [], null, null)
     ], null, null);
 }
-export var AppComponentNgFactory = import0.ɵccf('my-app', import7.AppComponent, View_AppComponent_Host_0, {}, {}, []);
+export var AppComponentNgFactory = import0.ɵccf('my-app', import5.AppComponent, View_AppComponent_Host_0, {}, {}, []);
